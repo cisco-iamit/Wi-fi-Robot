461ecf56a0eae245b415e86e0053404ce8f8c6c9
Vicky <vickyd885@gmail.com>
2017-09-07 17:12:04 +0000

    add extra sensor cases

--- webserver/automode.py
+++ webserver/automode.py
@@ -6,8 +6,16 @@ def run():
 
     while(1):
         sensor_left_val = robot.sensors.read_left_sensor()
+        sensor_right_val = robot.sensors.read_right_sensor()
+        sensor_front_val = robot.sensors.read_front_sensor()
+
+        
         if sensor_left_val == 0:
             robot.right()
+        elif sensor_right_val == 0:
+            robot.left()
+        elif sensor_front_val == 0:
+            robot.backward()
         else:
             robot.forward()
 
