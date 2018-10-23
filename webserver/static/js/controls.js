a4264828cdd4f11014318e7b4438d75c44490e97
Vicky Dineshchandra <vdineshc@cisco.com>
2018-10-23 14:29:48 +0100

    add get request to connect to server

--- webserver/static/js/controls.js
+++ webserver/static/js/controls.js
@@ -1,25 +1,40 @@
+function send_command(command){
+    $.get('/' + command, function(){
+        console.log("command sent!");
+    });
+}
+
 function moveForward(){
     // console.log("Moving forward");
     $('.fa-arrow-up').css('color','crimson');
+    send_command('move_forward');
 }
 
 function turnLeft(){
     // console.log("Turning Left");
     $('.fa-arrow-left').css('color','crimson');
+    send_command('turn_left');
 }
 
 function turnRight(){
     // console.log("Turning right");
     $('.fa-arrow-right').css('color','crimson');
+    send_command('turn_right');
 }
 
 function moveBack(){
     // console.log("Moving back");
     $('.fa-arrow-down').css('color','crimson');
+    send_command('move_back')
 }
 
 function resetArrowColours(){
     $('.fas').css('color','darkgray');
+}
+
+function stopRobot(){
+    resetArrowColours();
+    send_command('stop');
 }
 
 
@@ -48,7 +63,7 @@ document.onkeyup = function(e){
 
     if(arrowKeyCodes.has(keyCode)){
         // console.log("Arrow key released!")
-        resetArrowColours();
+        stopRobot();
     }
 }
 
@@ -57,24 +72,24 @@ $( document ).ready(function() {
     $('.fa-arrow-up').mousedown(function(){
         moveForward();
     }).mouseup(function(){
-        resetArrowColours();
+        stopRobot();
     });
 
     $('.fa-arrow-left').mousedown(function(){
         turnLeft();
     }).mouseup(function(){
-        resetArrowColours();
+        stopRobot();
     });
 
     $('.fa-arrow-right').mousedown(function(){
         turnRight();
     }).mouseup(function(){
-        resetArrowColours();
+        stopRobot();
     });
 
     $('.fa-arrow-down').mousedown(function(){
         moveBack();
     }).mouseup(function(){
-        resetArrowColours();
+        stopRobot();
     });
 });
