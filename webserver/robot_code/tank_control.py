cba1ee22b1aeb76c612c3d7fdf086467d7347e37
Vicky Dineshchandra <vdineshc@cisco.com>
2018-10-23 10:14:52 +0100

    add govinds robot code

---
+++ webserver/robot_code/tank_control.py
@@ -0,0 +1,35 @@
+from GPIO2 import RobotDriver
+
+class RobotController():
+
+    def __init__(self):
+        self.robot = RobotDriver()
+
+    def forward(self):
+        self.robot.left_motor_forward()
+        self.robot.right_motor_forward()
+        print("forward")
+
+
+    def left(self):
+        self.robot.left_motor_backward()
+        self.robot.right_motor_forward()
+        print("left")
+
+
+    def right(self):
+        self.robot.right_motor_backward()
+        self.robot.left_motor_forward()
+        print("right")
+
+
+    def backward(self):
+        self.robot.left_motor_backward()
+        self.robot.right_motor_backward()
+        print("backward")
+
+
+    def stop(self):
+        self.robot.left_motor_stop()
+        self.robot.right_motor_stop()
+        print("stop")
