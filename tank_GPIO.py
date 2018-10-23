0d9f04cef9b973a897ffb16dff3b8fe9ae8ed648
GovSohal <44172436+GovSohal@users.noreply.github.com>
2018-10-23 12:28:20 +0100

    Update tank_GPIO.py

--- tank_GPIO.py
+++ tank_GPIO.py
@@ -1,24 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-
-
 class RobotDriver():
 
     def __init__(self,
-    left_motor_pin_1=26,
-    left_motor_pin_2=20,
-    right_motor_pin_1=19,
-    right_motor_pin_2=17):
+        left_motor_pin_1=16,
+        left_motor_pin_2=19,
+        right_motor_pin_1=26,
+        right_motor_pin_2=20):
 
         self.left_motor_pin_1 = left_motor_pin_1
         self.left_motor_pin_2 = left_motor_pin_2
         self.right_motor_pin_1 = right_motor_pin_1
         self.right_motor_pin_2 = right_motor_pin_2
 
-        init_pins()
+        self.init_pins()
 
-    def init_pins():
+    def init_pins(self):
         GPIO.setmode(GPIO.BCM)              #set pin mode
         GPIO.setwarnings(False)             #turns off power limt warnings
         #left motor
@@ -29,28 +27,28 @@ class RobotDriver():
         GPIO.setup(self.right_motor_pin_2,GPIO.OUT)
 
 
-    def left_motor_forward():
-        GPIO.output(self.left_motor_pin_1,GPIO.high)
-        GPIO.output(self.left_motor_pin_1,GPIO.low)
+    def left_motor_forward(self):
+        GPIO.output(self.left_motor_pin_1,GPIO.HIGH)
+        GPIO.output(self.left_motor_pin_2,GPIO.LOW)
 
-    def left_motor_backward():
-        GPIO.output(self.right_motor_pin_1,GPIO.low)
-        GPIO.output(self.right_motor_pin_2,GPIO.high)
+    def left_motor_backward(self):
+        GPIO.output(self.left_motor_pin_1,GPIO.LOW)
+        GPIO.output(self.left_motor_pin_2,GPIO.HIGH)
 
-    def right_motor_stop():
-        GPIO.output(self.right_motor_pin_1,GPIO.low)
-        GPIO.output(self.right_motor_pin_2,GPIO.low)
+    def left_motor_stop(self):
+        GPIO.output(self.left_motor_pin_1,GPIO.LOW)
+        GPIO.output(self.left_motor_pin_2,GPIO.LOW)
 
 
 
-    def right_motor_forward():
-        GPIO.output(26,GPIO.high)
-        GPIO.output(20,GPIO.low)
+    def right_motor_forward(self):
+        GPIO.output(self.right_motor_pin_1,GPIO.HIGH)
+        GPIO.output(self.right_motor_pin_2,GPIO.LOW)
 
-    def right_motor_backward():
-        GPIO.output(26,GPIO.low)
-        GPIO.output(20,GPIO.high)
+    def right_motor_backward(self):
+        GPIO.output(self.right_motor_pin_1,GPIO.LOW)
+        GPIO.output(self.right_motor_pin_2,GPIO.HIGH)
 
-    def right_motor_stop():
-        GPIO.output(26,GPIO.low)
-        GPIO.output(20,GPIO.low)
+    def right_motor_stop(self):
+        GPIO.output(self.right_motor_pin_1,GPIO.LOW)
+        GPIO.output(self.right_motor_pin_2,GPIO.LOW)
