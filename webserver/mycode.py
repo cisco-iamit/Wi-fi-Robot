3289d6edf9206c9f9fb5ce369e804ed28c0da0dd
Jonty Preston <jsip201@exeter.ac.uk>
2018-11-12 11:23:21 +0000

    Update mycode.py

--- webserver/mycode.py
+++ webserver/mycode.py
@@ -1,11 +1,11 @@
-from  robot_code.tank_control import RobotController as controller
+from  robot_code.tank_control import RobotController
 import time
 
-robot = controller()
+robot = RobotController()
 
 def run():
 
-    while(1):
+    while True:
         sensor_front_val = robot.sensors.read_front_sensor()
         print("Front sensor: {}".format(sensor_front_pin))
 
